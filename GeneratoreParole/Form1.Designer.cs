3fb845b1d3ed00bf9369dfd272331f7d245d1c9e
RekTor29 <alessio.rizzo.studenti@isii.it>
2020-11-29 22:31:22 +0100

    Aggiungere i file di progetto.

---
+++ GeneratoreParole/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace GeneratoreParole
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.grpBox1 = new System.Windows.Forms.GroupBox();
+            this.grpBox2 = new System.Windows.Forms.GroupBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.pgrsBar1 = new System.Windows.Forms.ProgressBar();
+            this.pgrsBar2 = new System.Windows.Forms.ProgressBar();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.bgw1 = new System.ComponentModel.BackgroundWorker();
+            this.bgw2 = new System.ComponentModel.BackgroundWorker();
+            this.bgw3 = new System.ComponentModel.BackgroundWorker();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.bgw4 = new System.ComponentModel.BackgroundWorker();
+            this.grpBox1.SuspendLayout();
+            this.grpBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpBox1
+            // 
+            this.grpBox1.Controls.Add(this.txtBox1);
+            this.grpBox1.Controls.Add(this.lbl1);
+            this.grpBox1.Controls.Add(this.pgrsBar1);
+            this.grpBox1.Controls.Add(this.btn1);
+            this.grpBox1.Location = new System.Drawing.Point(12, 12);
+            this.grpBox1.Name = "grpBox1";
+            this.grpBox1.Size = new System.Drawing.Size(296, 96);
+            this.grpBox1.TabIndex = 0;
+            this.grpBox1.TabStop = false;
+            this.grpBox1.Text = "Thread Singolo";
+            // 
+            // grpBox2
+            // 
+            this.grpBox2.Controls.Add(this.txtBox2);
+            this.grpBox2.Controls.Add(this.lbl2);
+            this.grpBox2.Controls.Add(this.pgrsBar2);
+            this.grpBox2.Controls.Add(this.btn2);
+            this.grpBox2.Location = new System.Drawing.Point(12, 114);
+            this.grpBox2.Name = "grpBox2";
+            this.grpBox2.Size = new System.Drawing.Size(296, 96);
+            this.grpBox2.TabIndex = 1;
+            this.grpBox2.TabStop = false;
+            this.grpBox2.Text = "Tre Thread";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(13, 26);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 25);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Start";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(13, 19);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 25);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Start";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // pgrsBar1
+            // 
+            this.pgrsBar1.Location = new System.Drawing.Point(104, 26);
+            this.pgrsBar1.Name = "pgrsBar1";
+            this.pgrsBar1.Size = new System.Drawing.Size(172, 24);
+            this.pgrsBar1.TabIndex = 1;
+            // 
+            // pgrsBar2
+            // 
+            this.pgrsBar2.Location = new System.Drawing.Point(104, 19);
+            this.pgrsBar2.Name = "pgrsBar2";
+            this.pgrsBar2.Size = new System.Drawing.Size(172, 24);
+            this.pgrsBar2.TabIndex = 2;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(31, 66);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(40, 13);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Tempo";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(31, 61);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(40, 13);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "Tempo";
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(104, 58);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(172, 20);
+            this.txtBox2.TabIndex = 4;
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(104, 63);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(172, 20);
+            this.txtBox1.TabIndex = 5;
+            // 
+            // bgw1
+            // 
+            this.bgw1.WorkerReportsProgress = true;
+            this.bgw1.WorkerSupportsCancellation = true;
+            this.bgw1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw1_DoWork);
+            this.bgw1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw1_ProgressChanged);
+            this.bgw1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw1_RunWorkerCompleted);
+            // 
+            // bgw2
+            // 
+            this.bgw2.WorkerReportsProgress = true;
+            this.bgw2.WorkerSupportsCancellation = true;
+            this.bgw2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw2_DoWork);
+            this.bgw2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw2_ProgressChanged);
+            this.bgw2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw2_RunWorkerCompleted);
+            // 
+            // bgw3
+            // 
+            this.bgw3.WorkerReportsProgress = true;
+            this.bgw3.WorkerSupportsCancellation = true;
+            this.bgw3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw3_DoWork);
+            this.bgw3.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw3_ProgressChanged);
+            this.bgw3.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw3_RunWorkerCompleted);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(12, 216);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(296, 29);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Gara!";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // bgw4
+            // 
+            this.bgw4.WorkerReportsProgress = true;
+            this.bgw4.WorkerSupportsCancellation = true;
+            this.bgw4.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw4_DoWork);
+            this.bgw4.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgw4_ProgressChanged);
+            this.bgw4.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgw4_RunWorkerCompleted);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 251);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.grpBox2);
+            this.Controls.Add(this.grpBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Generatore Di Parole";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.grpBox1.ResumeLayout(false);
+            this.grpBox1.PerformLayout();
+            this.grpBox2.ResumeLayout(false);
+            this.grpBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpBox1;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.ProgressBar pgrsBar1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.GroupBox grpBox2;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.ProgressBar pgrsBar2;
+        private System.Windows.Forms.Button btn2;
+        private System.ComponentModel.BackgroundWorker bgw1;
+        private System.ComponentModel.BackgroundWorker bgw2;
+        private System.ComponentModel.BackgroundWorker bgw3;
+        private System.Windows.Forms.Button btn3;
+        private System.ComponentModel.BackgroundWorker bgw4;
+    }
+}
+
---
+++ GeneratoreParole/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+//Scritto da Rizzo Alessio 4^G
+//Programma che genera 1000 parole da dieci lettere su un singolo o su multipli Thread
+//Note:
+namespace GeneratoreParole
+{
+    public partial class frmMain : Form
+    {
+        //Dichiarazione StopWatch
+        Stopwatch stopwatch1 = new Stopwatch();
+        Stopwatch stopwatch2 = new Stopwatch();
+
+        //Random Array che genera 10000 lettere che andranno a comporre parole da 10 lettere
+        Random rnd = new Random();
+
+        //Implementazione Liste
+        List<string> PAROLE1;
+        List<string> PAROLE2;
+        public frmMain()
+        {
+            InitializeComponent();       
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //Clear la textbox
+            txtBox1.Clear();
+            //Pulisco StopWatch
+            stopwatch1 = new Stopwatch();
+            //Viene creata una nuova lista
+            PAROLE1 = new List<string>();
+            //Quando il bottone viene premuto lo stopwatch inizia a contare
+            stopwatch1.Start();
+            //Viene fatto partire il BGW
+            bgw1.RunWorkerAsync();                     
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            //Clear la textbox
+            txtBox2.Clear();
+            // Pulisco StopWatch
+            stopwatch2 = new Stopwatch();
+            //Viene creata una nuova lista
+            PAROLE2 = new List<string>();                     
+            //Vengono fatti partire i BGW
+            bgw2.RunWorkerAsync();
+            bgw3.RunWorkerAsync();
+            bgw4.RunWorkerAsync();
+            //Quando il bottone viene premuto lo stopwatch inizia a contare
+            stopwatch2.Start();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            //Se il bottone "Gara" viene cliccato allora è come se i due bottoni start venissero cliccati insieme
+            btn1_Click(sender, e);
+            btn2_Click(sender, e);          
+        }
+
+        private void bgw1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //Mentre gli elementi della lista PAROLE1 sono minori o uguali di 1000
+            while (PAROLE1.Count <= 1000)
+            {
+                //La lista PAROLE1 aggiunge elementi richiamando il metodo GeneraParole
+                PAROLE1.Add(GeneratoreDiParole());
+
+                bgw1.ReportProgress(PAROLE1.Count / 10);
+                Thread.Sleep(10);
+            }
+        }
+
+        private void bgw1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgrsBar1.Value = e.ProgressPercentage;
+        }
+
+        private void bgw1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            bgw1.CancelAsync();
+            stopwatch1.Stop();
+            txtBox1.Text = stopwatch1.ElapsedMilliseconds.ToString();           
+        }
+
+        private void bgw2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgw = sender as BackgroundWorker;
+
+            while (PAROLE2.Count <= 1000)
+            {
+                lock (PAROLE2)
+                {
+                    PAROLE2.Add(GeneratoreDiParole());
+                }
+                bgw.ReportProgress(PAROLE2.Count / 10);
+                Thread.Sleep(10);
+            }
+        }
+
+        private void bgw2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgrsBar2.Value = e.ProgressPercentage;
+        }
+
+        private void bgw2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //Fermo i tre BGW
+            bgw2.CancelAsync();            
+            //Fermo lo StopWatch2
+            stopwatch2.Stop();
+            //Stampo nella textBox2 il tempo in Millisecondi
+            txtBox2.Text = stopwatch2.ElapsedMilliseconds.ToString();            
+        }
+
+        //Metodo che genera parole di 10 caratteri
+        public string GeneratoreDiParole()
+        {
+            string parole = "";
+            for (int j = 0; j < 10; j++)
+            {
+                parole += Convert.ToChar(rnd.Next(65, 98));
+            }
+            return parole;
+        }
+
+        private void bgw3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgw = sender as BackgroundWorker;
+
+            while (PAROLE2.Count <= 1000)
+            {
+                lock (PAROLE2)
+                {
+                    PAROLE2.Add(GeneratoreDiParole());
+                }
+                bgw.ReportProgress(PAROLE2.Count / 3);
+                Thread.Sleep(10);
+            }
+        }
+        private void bgw3_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+           
+        }
+
+        private void bgw3_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            bgw3.CancelAsync();
+        }
+        private void bgw4_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgw = sender as BackgroundWorker;
+
+            while (PAROLE2.Count <= 1000)
+            {
+                lock (PAROLE2)
+                {
+                    PAROLE2.Add(GeneratoreDiParole());
+                }
+                bgw.ReportProgress(PAROLE2.Count / 3);
+                Thread.Sleep(10);
+            }
+        }       
+
+        private void bgw4_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            
+        }
+
+        private void bgw4_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            bgw4.CancelAsync();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            txtBox1.Enabled = false;
+            txtBox2.Enabled = false;
+        }
+    }
+}
